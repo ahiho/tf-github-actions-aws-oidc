8c244ece72774ff6c5ba5e48609af30249c5cf12
Long Le <levanlongktmt@gmail.com>
2023-07-20 13:45:02 +0700

    fix: optional field

--- variables.tf
+++ variables.tf
@@ -18,6 +18,6 @@ variable "oidc_provider_arn" {
 }
 
 variable "repo_policies" {
-  type        = map(object({ inline_policy = string, managed_policy_arns = list(string) }))
+  type        = map(object({ inline_policy = optional(string), managed_policy_arns = optional(list(string)) }))
   description = "Repository name and policy ARN key/value pairs"
 }
