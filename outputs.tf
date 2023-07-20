ff71879971d6b3738c0e5912916f5f8f184a4dbd
Long Le <levanlongktmt@gmail.com>
2023-07-20 13:33:37 +0700

    fix: wrong output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "iam_role_arns" {
-  depends_on  = [aws_iam_role.github]
+  depends_on  = [aws_iam_role.github_actions_role]
   description = "ARN of create IAM roles"
-  value       = [for o in aws_iam_role.github : o.arn]
+  value       = [for o in aws_iam_role.github_actions_role : o.arn]
 }
