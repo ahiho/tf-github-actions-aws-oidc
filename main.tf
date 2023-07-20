52f442c1a5885a6932d46fdc1ebe16321083fe3e
Long Le <levanlongktmt@gmail.com>
2023-07-20 18:12:51 +0700

    fix: something

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 resource "aws_iam_role" "github_actions_role" {
   for_each = {
     for index, rp in var.repo_policies :
-    sha1("${rp.repo}/${join(",", length(rp.branches) == 0 ? ["*"] : rp.branches)}") => rp
+    sha1("${rp.repo}/${join(",", coalescelist(rp.branches, ["*"]))}") => rp
   }
 
-  name = "GithubAction_Role_${replace(each.value.repo, "/", "@")}_${md5(join(",", length(each.value.branches) == 0 || contains(each.value.branches, "*") ? ["*"] : each.value.branches))}"
+  name = "GithubAction_Role_${replace(each.value.repo, "/", "@")}_${md5(join(",", coalescelist(rp.branches, ["*"])))}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -26,7 +26,7 @@ resource "aws_iam_role" "github_actions_role" {
         }
         Condition = {
           StringLike = {
-            "token.actions.githubusercontent.com:sub" : length(each.value.branches) == 0 || contains(each.value.branches, "*") ? "${each.value.repo}:*" : [for branch in each.value.branches : "${each.value.repo}:ref:refs/heads/${branch}"]
+            "token.actions.githubusercontent.com:sub" : contains(coalescelist(rp.branches, ["*"]), "*") ? "${each.value.repo}:*" : [for branch in each.value.branches : "${each.value.repo}:ref:refs/heads/${branch}"]
           }
           StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
@@ -39,7 +39,7 @@ resource "aws_iam_role" "github_actions_role" {
   managed_policy_arns = each.value.managed_policy_arns
 
   dynamic "inline_policy" {
-    for_each = length(each.value.inline_policy) > 0 ? [each.value.inline_policy] : []
+    for_each = each.value.inline_policy != null && length(each.value.inline_policy) > 0 ? [each.value.inline_policy] : []
     content {
       name   = "GithubAction_Policy_${replace(each.key, "/", "@")}"
       policy = inline_policy.value
